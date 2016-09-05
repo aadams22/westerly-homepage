2debe235eca2ba170a51602d7573062c8b381116
aadams22 <ashleigh.adams22@gmail.com>
2016-09-05 14:50:55 -0500

    changing indentation settings on editor

--- js/animation.js
+++ js/animation.js
@@ -2,49 +2,49 @@
 
 $(function(){
 
-	//Hiding all elements on screen to be shown later
-	$('#second').hide();
-	$('#third').hide();
-	$('#fourth').hide();
-	$('#fifth').hide();
-	$('#sixth').hide();
-	$('#seventh').hide();
-	$('#eigth').hide();
+  //Hiding all elements on screen to be shown later
+  $('#second').hide();
+  $('#third').hide();
+  $('#fourth').hide();
+  $('#fifth').hide();
+  $('#sixth').hide();
+  $('#seventh').hide();
+  $('#eigth').hide();
 
-	//Slideshow
-	function slideshow(){
-		$('#first').delay(2000).fadeOut(1000);
-		$('#second').delay(3000).fadeIn(1000);
+  //Slideshow
+  function slideshow(){
+    $('#first').delay(2000).fadeOut(1000);
+    $('#second').delay(3000).fadeIn(1000);
 
-		$('#second').delay(2000).fadeOut(1000);
-		$('img').delay(6000).fadeOut(1000);
+    $('#second').delay(2000).fadeOut(1000);
+    $('img').delay(6000).fadeOut(1000);
 
-		$('#third').delay(7000).fadeIn(1000);
-		$('#third').delay(3000).fadeOut(1000);
+    $('#third').delay(7000).fadeIn(1000);
+    $('#third').delay(3000).fadeOut(1000);
 
-		$('#fourth').delay(12000).fadeIn(1000);
-		$('#fifth').delay(12500).fadeIn(1000);
-		$('#sixth').delay(13000).fadeIn(1000);
+    $('#fourth').delay(12000).fadeIn(1000);
+    $('#fifth').delay(12500).fadeIn(1000);
+    $('#sixth').delay(13000).fadeIn(1000);
 
-		$('#fourth').delay(3000).fadeOut(1000);
-		$('#fifth').delay(3000).fadeOut(1000);
-		$('#sixth').delay(3000).fadeOut(1000);
+    $('#fourth').delay(3000).fadeOut(1000);
+    $('#fifth').delay(3000).fadeOut(1000);
+    $('#sixth').delay(3000).fadeOut(1000);
 
-		$('#seventh').delay(18000).fadeIn(1000);
-		$('#eigth').delay(19000).fadeIn(1000);
+    $('#seventh').delay(18000).fadeIn(1000);
+    $('#eigth').delay(19000).fadeIn(1000);
 
-		$('#seventh').delay(10000).fadeOut(1000);
-		$('#eigth').delay(10000).fadeOut(1000);
+    $('#seventh').delay(10000).fadeOut(1000);
+    $('#eigth').delay(10000).fadeOut(1000);
 
-		$('img').delay(24000).fadeIn(1000);
-		$('#first').delay(28000).fadeIn(1000);
-	}
+    $('img').delay(24000).fadeIn(1000);
+    $('#first').delay(28000).fadeIn(1000);
+  }
 
-	//Runs slideshow for the first time.
-	slideshow();
+  //Runs slideshow for the first time.
+  slideshow();
 
-	//Repeats slideshow
-	setInterval(slideshow, 40000);
+  //Repeats slideshow
+  setInterval(slideshow, 40000);
 
 
 });
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var morgan = require('morgan');
-var ngrok = require('ngrok');
+var morgan  = require('morgan');
+var ngrok   = require('ngrok');
 
-app = express();
+var app     = express();
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
@@ -14,8 +14,8 @@ app.listen(3000,function(){
 // ngrok http -auth="westerly:doodle" 3000
 
 ngrok.connect({
-	proto: 'http',
-	addr: 3000,
-	auth: 'user:pwd',
-	region: 'us'
+  proto: 'http',
+  addr: 3000,
+  auth: 'user:pwd',
+  region: 'us'
 }, function(err, url){});
